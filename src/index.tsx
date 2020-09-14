71016892dd335839a9f2d7d125ec12b83dd966cd
kad-overj <jorrit.overeem@kadaster.nl>
2020-09-14 09:04:18 +0200

    commit project from diffeent repe

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "containers/App";
+import ContextProviders from "ContextProviders";
+import registerIcons from "./utils/FaIcons";
+
+/* Index  */
+registerIcons();
+ReactDOM.render(
+  <ContextProviders>
+    <App />
+  </ContextProviders>,
+  document.getElementById("app")
+);
