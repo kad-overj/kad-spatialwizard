8ae80da743deac2490a6ae5057bc293ce637168d
kad-overj <jorrit.overeem@kadaster.nl>
2020-09-22 09:19:29 +0200

    Started over with the selectbox

--- src/containers/Publish/SourceSelector.tsx
+++ src/containers/Publish/SourceSelector.tsx
@@ -1,15 +1,13 @@
-import * as React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import InputLabel from "@material-ui/core/InputLabel";
+import FormHelperText from "@material-ui/core/FormHelperText";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
+import NativeSelect from "@material-ui/core/NativeSelect";
 
-export interface Props {}
-
-export interface State {}
-
-export default function SourceSelector() {
-  const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
     formControl: {
       margin: theme.spacing(1),
       minWidth: 120,
@@ -17,36 +15,75 @@ export default function SourceSelector() {
     selectEmpty: {
       marginTop: theme.spacing(2),
     },
-  }));
+  })
+);
 
+export default function SourceSelector() {
   const classes = useStyles();
-  const [state, setState] = React.useState({
-    source: "",
+  const [state, setState] = React.useState<{ age: string | number; name: string }>({
+    age: "",
+    name: "hai",
   });
 
-  const handleChange = (event) => {
-    const name = event.target.name;
+  const handleChange = (event: React.ChangeEvent<{ name?: string; value: unknown }>) => {
+    const name = event.target.name as keyof typeof state;
     setState({
       ...state,
       [name]: event.target.value,
     });
   };
+
   return (
-    <FormControl className={classes.formControl}>
-      <InputLabel htmlFor="source-native-simple">Token source</InputLabel>
-      <Select
-        native
-        value={state.source}
-        onChange={handleChange}
-        inputProps={{
-          name: "source",
-          id: "source-native-simple",
-        }}
-      >
-        <option aria-label="None" value="" />
-        <option value={10}>PLDN</option>
-        <option value={20}>Kadaster</option>
-      </Select>
-    </FormControl>
+    <div>
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="age-native-simple">Age</InputLabel>
+        <Select
+          native
+          value={state.age}
+          onChange={handleChange}
+          inputProps={{
+            name: "age",
+            id: "age-native-simple",
+          }}
+        >
+          <option aria-label="None" value="" />
+          <option value={10}>Ten</option>
+          <option value={20}>Twenty</option>
+          <option value={30}>Thirty</option>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="age-native-helper">Age</InputLabel>
+        <NativeSelect
+          value={state.age}
+          onChange={handleChange}
+          inputProps={{
+            name: "age",
+            id: "age-native-helper",
+          }}
+        >
+          <option aria-label="None" value="" />
+          <option value={10}>Ten</option>
+          <option value={20}>Twenty</option>
+          <option value={30}>Thirty</option>
+        </NativeSelect>
+        <FormHelperText>Some important helper text</FormHelperText>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <NativeSelect
+          value={state.age}
+          onChange={handleChange}
+          name="age"
+          className={classes.selectEmpty}
+          inputProps={{ "aria-label": "age" }}
+        >
+          <option value="">None</option>
+          <option value={10}>Ten</option>
+          <option value={20}>Twenty</option>
+          <option value={30}>Thirty</option>
+        </NativeSelect>
+        <FormHelperText>With visually hidden label</FormHelperText>
+      </FormControl>
+    </div>
   );
 }
